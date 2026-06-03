0962fbdbf6133a214fb42f11896e63e5c05eb125
Nova <nova@novatea.dev>
2026-06-03 00:07:38 -0600

    typescript: start writing auth handlers and improve some package protections meow mrrp

--- typescript/sdk.d.ts
+++ typescript/sdk.d.ts
@@ -1,4 +1,4 @@
 export interface FolderHarborConfig {
   server: string;
-  key?: string | undefined;
+  token?: string | undefined;
 }
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -2,20 +2,20 @@ import type { FolderHarborConfig } from "../sdk";
 import { FHConfigError, FHRequestError, FHUserError } from "./errors";
 
 export class FolderHarbor {
-  private server: string;
-  private apiKey: string | undefined;
+  #server: string;
+  #token: string | undefined;
   constructor(config: FolderHarborConfig) {
     if (!config.server) throw new FHConfigError("Invalid server address");
-    this.server = config.server;
-    if (config.key) this.apiKey = config.key;
+    this.#server = config.server;
+    if (config.token) this.#token = config.token;
   }
   async request<T>(path: string, init?: RequestInit): Promise<T> {
     let body: object | undefined;
-    const url = new URL(this.server);
+    const url = new URL(this.#server);
     url.pathname += path.split("?")[0];
     if (path.split("?").length >= 2) url.search += path.split("?")[1];
     const headers = new Headers();
-    if (this.apiKey) headers.append("Authorization", `Bearer ${this.apiKey}`);
+    if (this.#token) headers.append("Authorization", `Bearer ${this.#token}`);
     headers.append("Content-Type", "application/json");
     headers.append("Accept", "application/json");
     try {
@@ -34,5 +34,14 @@ export class FolderHarbor {
       }
     }
     return body as T;
+  }
+
+  public auth = {
+    login: async (body: { username: string, password: string }): Promise<{ token: string }> => {
+      
+    },
+    logout: async () => {
+
+    }
   }
 }
